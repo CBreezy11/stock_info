e2479fde7b5fb5827447cb49df13724ba675e3d7
Brandon Leser <brandon.leser@gmail.com>
2018-06-26 01:37:41 -0700

    finished program to upload News Headlines to dynamo for top stocks

--- stock_info.py
+++ stock_info.py
@@ -1,12 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
+import boto3
+import datetime
 
-page = requests.get('https://www.insider-monitor.com/insider_stock_trading_report.html')
-soup = BeautifulSoup(page.text, 'html.parser')
-base = soup.find(class_='table table-condensed')
-ticker = base.find_all('a')
-for a in ticker:
-    print(a.text)
+class DynamoStock(object):
+    def __init__(self):
+        self.dynamodb_resource = boto3.resource('dynamodb')
+
+    def add_item(self, data_dict):
+        self.dynamodb_resource.Table('InsiderTrading').put_item( Item = data_dict)
+        
+class Traders(DynamoStock):
+    def __init__(self):
+        super().__init__()
+        self.url = 'https://finance.yahoo.com/quote/{}/press-releases?p={}'
+        self.stock_list =["FB", "AAPL", "GOOG", "MSFT", "BABA", "NFLX", "NVDA", "INTC",]
+        self.timenow = datetime.datetime.now().strftime("%m-%d-%Y")
+
+    def compile(self):
+        for stock in self.stock_list:
+            self.get_traders(stock)
+            
+
+    def get_traders(self, stock_name):
+        url = self.url.format(stock_name, stock_name)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        base = soup.find('u')
+        news = base.find_next()
+        item_dict = {'{} News'.format(self.timenow):news.text}
+        item_dict.update({'company_name': stock_name})
+        self.add_item(item_dict)
+   
+
+
+
 
 
 
